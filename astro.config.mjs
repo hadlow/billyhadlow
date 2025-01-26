a0d5a3be4fe913cf2e82c8ca459d81a4b4bde0d7
Billy Hadlow <me@billyhadlow.com>
2025-01-26 07:56:47 -0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,18 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown'
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://billyhadlow.com',
   integrations: [
     tailwind(),
     partytown({
       config: {
         forward: ["dataLayer.push"],
       },
-    })
+    }),
+    sitemap(),
   ]
 });
