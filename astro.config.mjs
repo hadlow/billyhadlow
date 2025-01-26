1a60d4c22786a83911c2ce4c0498e4ecea7d5fef
Billy Hadlow <me@billyhadlow.com>
2025-01-25 20:09:58 -0800

    add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import partytown from '@astrojs/partytown'
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
+  ]
 });
